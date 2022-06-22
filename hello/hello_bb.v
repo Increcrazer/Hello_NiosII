66223d256d69bf72cb1d29e6605feee223b8f3b2
Increcrazer <1759991046@qq.com>
2022-06-23 00:11:24 +0800

    添加所有文件

---
+++ hello/hello_bb.v
@@ -0,0 +1,8 @@
+
+module hello (
+	clk_0_clk,
+	reset_reset_n);	
+
+	input		clk_0_clk;
+	input		reset_reset_n;
+endmodule
---
+++ hello/hello_inst.v
@@ -0,0 +1,5 @@
+	hello u0 (
+		.clk_0_clk     (<connected-to-clk_0_clk>),     // clk_0.clk
+		.reset_reset_n (<connected-to-reset_reset_n>)  // reset.reset_n
+	);
+
