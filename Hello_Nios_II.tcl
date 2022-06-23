da80863b455e514baca9e27646f1af093909c8fb
Increcrazer <1759991046@qq.com>
2022-06-23 13:17:57 +0800

    加入.tcl

---
+++ Hello_Nios_II.tcl
@@ -0,0 +1,77 @@
+# Copyright (C) 1991-2015 Altera Corporation. All rights reserved.
+# Your use of Altera Corporation's design tools, logic functions 
+# and other software and tools, and its AMPP partner logic 
+# functions, and any output files from any of the foregoing 
+# (including device programming or simulation files), and any 
+# associated documentation or information are expressly subject 
+# to the terms and conditions of the Altera Program License 
+# Subscription Agreement, the Altera Quartus II License Agreement,
+# the Altera MegaCore Function License Agreement, or other 
+# applicable license agreement, including, without limitation, 
+# that your use is for the sole purpose of programming logic 
+# devices manufactured by Altera and sold by Altera or its 
+# authorized distributors.  Please refer to the applicable 
+# agreement for further details.
+
+# Quartus II: Generate Tcl File for Project
+# File: Hello_Nios_II.tcl
+# Generated on: Thu Jun 23 13:09:31 2022
+
+# Load Quartus II Tcl Project package
+package require ::quartus::project
+
+set need_to_close_project 0
+set make_assignments 1
+
+# Check that the right project is open
+if {[is_project_open]} {
+	if {[string compare $quartus(project) "Hello_Nios_II"]} {
+		puts "Project Hello_Nios_II is not open"
+		set make_assignments 0
+	}
+} else {
+	# Only open if not already open
+	if {[project_exists Hello_Nios_II]} {
+		project_open -revision Hello_Nios_II Hello_Nios_II
+	} else {
+		project_new -revision Hello_Nios_II Hello_Nios_II
+	}
+	set need_to_close_project 1
+}
+
+# Make assignments
+if {$make_assignments} {
+	set_global_assignment -name FAMILY "Cyclone IV E"
+	set_global_assignment -name DEVICE EP4CE40F23C8
+	set_global_assignment -name ORIGINAL_QUARTUS_VERSION 15.0.0
+	set_global_assignment -name PROJECT_CREATION_TIME_DATE "17:43:34  JUNE 22, 2022"
+	set_global_assignment -name LAST_QUARTUS_VERSION 15.0.0
+	set_global_assignment -name PROJECT_OUTPUT_DIRECTORY output_files
+	set_global_assignment -name MIN_CORE_JUNCTION_TEMP 0
+	set_global_assignment -name MAX_CORE_JUNCTION_TEMP 85
+	set_global_assignment -name DEVICE_FILTER_PACKAGE FBGA
+	set_global_assignment -name DEVICE_FILTER_SPEED_GRADE 8
+	set_global_assignment -name ERROR_CHECK_FREQUENCY_DIVISOR 1
+	set_global_assignment -name NOMINAL_CORE_SUPPLY_VOLTAGE 1.2V
+	set_global_assignment -name EDA_SIMULATION_TOOL "ModelSim-Altera (Verilog)"
+	set_global_assignment -name EDA_OUTPUT_DATA_FORMAT "VERILOG HDL" -section_id eda_simulation
+	set_global_assignment -name BDF_FILE Hello_Nios_II.bdf
+	set_global_assignment -name QIP_FILE pll.qip
+	set_global_assignment -name PARTITION_NETLIST_TYPE SOURCE -section_id Top
+	set_global_assignment -name PARTITION_FITTER_PRESERVATION_LEVEL PLACEMENT_AND_ROUTING -section_id Top
+	set_global_assignment -name PARTITION_COLOR 16764057 -section_id Top
+	set_global_assignment -name POWER_PRESET_COOLING_SOLUTION "23 MM HEAT SINK WITH 200 LFPM AIRFLOW"
+	set_global_assignment -name POWER_BOARD_THERMAL_MODEL "NONE (CONSERVATIVE)"
+	set_global_assignment -name STRATIX_DEVICE_IO_STANDARD "2.5 V"
+	set_location_assignment PIN_G1 -to iCLk
+	set_location_assignment PIN_G21 -to iRSTn
+	set_instance_assignment -name PARTITION_HIERARCHY root_partition -to | -section_id Top
+
+	# Commit assignments
+	export_assignments
+
+	# Close project
+	if {$need_to_close_project} {
+		project_close
+	}
+}
